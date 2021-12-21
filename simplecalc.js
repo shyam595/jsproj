2eb3603c519ed0bd2ad99e96793862cb4c432fb9
shyam595 <46529933+shyam595@users.noreply.github.com>
2021-12-22 00:44:44 +0530

    basic calc commit

---
+++ simplecalc.js
@@ -0,0 +1,29 @@
+
+
+
+function calculate(){
+    var num1=document.getElementById("num1")
+    var num2=document.getElementById("num2")
+    var op=document.getElementById("operator")
+    var button=document.getElementById("button")
+    var result=document.getElementById("result")
+    if(num1.value==="" || num2.value===""){
+        alert("Please make sure you have entered both the operands")
+        result.value = "Number not Entered"
+    }
+    num1=Number(num1.value)
+    num2=Number(num2.value)
+    
+    
+if( op.value === '+' )
+{
+    result.value = num1 + num2
+}else if( op.value === '-' ){
+    result.value = num1 - num2
+}else if( op.value === '*' ){
+    result.value = num1 * num2
+}else{
+    result.value = num1 / num2
+}
+    
+}
