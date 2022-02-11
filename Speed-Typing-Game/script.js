e5f8ca2a112d89cf6c7d2bd949b6f00b4474b274
shyam595 <46529933+shyam595@users.noreply.github.com>
2022-02-11 22:13:27 +0530

    commit

---
+++ Speed-Typing-Game/script.js
@@ -0,0 +1,64 @@
+const random_quote_API = 'https://type.fit/api/quotes'
+
+const quoteDisplay = document.getElementById("quoteDisplay")
+const quoteInput = document.getElementById("quoteInput")
+const timer = document.getElementById("timer")
+
+quoteInput.addEventListener('input', ()=>{
+    const arrOfQuote = quoteDisplay.querySelectorAll("span")
+    const arrOfInput = quoteInput.value.split('')
+
+    let correct = true
+    arrOfQuote.forEach((charSpan , index) =>{
+        const charachter = arrOfInput[index]
+        if(charachter == null){
+            charSpan.classList.remove("correct")
+            charSpan.classList.remove("incorrect")
+            correct = false
+        }else if(charachter === charSpan.innerText){
+            charSpan.classList.add("correct")
+            charSpan.classList.remove("incorrect")
+            correct = true
+        }else{
+            charSpan.classList.remove("correct")
+            charSpan.classList.add("incorrect")
+            correct = false
+        }
+    })
+
+    if(correct) getNextQuote()
+})
+
+async function getRandomQuote(){
+    const response = await fetch(random_quote_API)
+    apiQuotes = await response.json()
+    return apiQuotes[Math.floor(Math.random()*apiQuotes.length)].text
+}
+
+async function getNextQuote(){
+    const quote = await getRandomQuote()
+    quoteDisplay.innerHTML = ''
+    quote.split('').forEach(charachter => {   
+        const charSpan = document.createElement("span")
+        charSpan.innerText = charachter
+        quoteDisplay.appendChild(charSpan)
+    })
+    quoteInput.value = null
+    startTimer()
+
+} 
+
+let startTime
+function startTimer(){
+    timer.innerText = 0
+    startTime = new Date
+    setInterval(()=> {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime(){
+    return Math.floor((new Date() - startTime )/1000)
+}
+
+getNextQuote()
---
+++ tic-tac-toe/script.js
@@ -0,0 +1,107 @@
+const submit = document.getElementById("submit");
+const game = document.querySelector(".game");
+const details = document.querySelector(".details");
+const message = document.querySelector(".message");
+
+let player1 = "";
+let player2 = "";
+let playGame = true;
+let activePlayer = 0;
+// 0 for player 1 and 1 for player 2
+
+function showMessage(msg) {
+  message.innerHTML = `<h3> ${msg}</h3>`;
+}
+
+function updateActivePlayer(){
+    if(activePlayer===0)
+        activePlayer=1
+    else
+        activePlayer=0
+}
+
+function initializeGame() {
+  player1 = document.getElementById("player1").value;
+  player2 = document.getElementById("player2").value;
+
+  activePlayer = 0;
+
+  details.style.display = "none";
+  game.style.display = "flex";
+
+  // Append game-slots to game div
+  showMessage(`${activePlayer === 0 ? player1 : player2}, you're up`);
+
+  for (let i = 1; i <= 9; i++) {
+    const div = document.createElement("div");
+
+    div.id = i;
+    div.classList.add("game-slot");
+
+    // Add event listener for clicking
+    div.addEventListener("click", function () {
+
+      if(!div.innerText && playGame){
+        div.innerText = activePlayer === 0 ? "x" : "o";
+        const winner = checkWinner()
+
+        if(!winner){
+          updateActivePlayer();
+          showMessage(`${activePlayer === 0 ? player1 : player2}, you're up`)
+        }else{
+          showMessage(`${activePlayer===0 ? player1: player2}, congratulations you won!`)
+          setTimeout(resetGame, 3000)
+        }
+      }
+
+
+    });
+
+    game.appendChild(div);
+  }
+}
+
+const winningSequences = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+function checkWinner(){
+    let winnerBool = false
+
+    for(let i=0;i<winningSequences.length;i++){
+      const winningCombo = winningSequences[i];
+
+      const cell1 = document.getElementById(winningCombo[0]+1)
+      const cell2 = document.getElementById(winningCombo[1]+1)
+      const cell3 = document.getElementById(winningCombo[2]+1)
+
+      const val1 = cell1.innerText
+      const val2 = cell2.innerText
+      const val3 = cell3.innerText
+
+      if(val1===val2 && val2===val3 && val1!=''){
+        winnerBool = true
+        playGame = false
+        cell1.style.backgroundColor="purple"
+        cell2.style.backgroundColor="purple"
+        cell3.style.backgroundColor="purple"
+        break;
+      }
+    }
+    return winnerBool
+}
+
+function resetGame(){
+    details.style.display = "block"
+    game.style.display = "none"
+    message.style.display = "none"
+    document.getElementById("player1").value = ""
+    document.getElementById("player2").value = ""
+}
+submit.addEventListener("click", initializeGame);
